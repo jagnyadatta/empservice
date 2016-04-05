9dca5c700000252efd901ffb3f9b829bc3c04f93
Jagnya Datta <jdatta3@sapient.com>
2016-04-05 21:24:12 +0530

    Add security

---
+++ employeeService/com.self.emp/com/self/conf/SpringAppInitializer.java
@@ -0,0 +1,7 @@
+package com.self.conf;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringAppInitializer extends AbstractSecurityWebApplicationInitializer {
+	   //do nothing
+	}
